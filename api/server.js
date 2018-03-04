ebcdf224c563493b8921fcf94b22604dda74a859
Ivan Villa <trezeguet55@gmail.com>
2018-03-04 12:19:56 -0500

    <api server.js>
    configurate number routes

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const mongo = require('./db/mongo');
+const NumberRoutes = require('./routes/number');
 
 const app = express();
+
+app.use(bodyParser.json());
 const port = process.env.PORT || 8000;
+
+mongo.setUpDB();
+
+const numberRoutes = new NumberRoutes(app);
+numberRoutes.setUpRoutes();
 
 app.listen(port, () => {console.log(`server running on the port ${port}`)});
