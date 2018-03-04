479bf1cdf348d213fb0036a79e01bfc4c6370306
Ivan Villa <trezeguet55@gmail.com>
2018-03-04 07:26:07 -0500

    <server.js>
    create basic server

---
+++ api/server.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {console.log(`server running on the port ${port}`)});
